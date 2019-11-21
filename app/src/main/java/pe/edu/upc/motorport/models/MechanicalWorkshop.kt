af5a35006f9bf4bf37884943538e16b1cc261576
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2019-11-20 21:01:27 -0500

    [#168799065] Added model: MechanicalWorkshop

---
+++ app/src/main/java/pe/edu/upc/motorport/models/MechanicalWorkshop.kt
@@ -0,0 +1,56 @@
+package pe.edu.upc.motorport.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "mechanical_workshops")
+class MechanicalWorkshop {
+    @PrimaryKey
+    @SerializedName("id")
+    var id: Int? = null
+
+    @ColumnInfo(name="name")
+    @SerializedName("name")
+    var name: String? = null
+
+    @ColumnInfo(name="image_url")
+    @SerializedName("imageUrl")
+    var imageUrl: String? = null
+
+    @ColumnInfo(name="average_rate")
+    @SerializedName("averageRate")
+    var averageRate: Double? = null
+
+    @SerializedName("street")
+    var street: String? = null
+
+    @ColumnInfo(name="street_number")
+    @SerializedName("streetNumber")
+    var streetNumber: String? = null
+
+    @ColumnInfo(name="zip_code")
+    @SerializedName("zipCode")
+    var zipCode: String? = null
+
+    @ColumnInfo(name="city")
+    @SerializedName("city")
+    var city: String? = null
+
+    @ColumnInfo(name="department")
+    @SerializedName("department")
+    var department: String? = null
+
+    @ColumnInfo(name="country")
+    @SerializedName("country")
+    var country: String? = null
+
+    @ColumnInfo(name="latitude")
+    @SerializedName("latitude")
+    var latitude: Double? = null
+
+    @ColumnInfo(name="longitude")
+    @SerializedName("longitude")
+    var longitude: Double? = null
+}
